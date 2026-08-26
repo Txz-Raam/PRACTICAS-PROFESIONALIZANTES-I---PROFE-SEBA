f1745224dda64fc0605b18f374ade201cc130272
Robertino Morales <164657612+Txz-Raam@users.noreply.github.com>
2026-08-26 16:21:08 -0300

    node.js

---
+++ HTML5/JavaScript/script.js
@@ -0,0 +1,37 @@
+//tipos de datos y variables
+//variable asignada
+let entero=10;
+let decimal=10.5;
+let caracter = "Practicas";
+let logico = true;
+
+//Mostrar
+console.log("El numero entero es: ",entero);
+console.log("El numero decimal es: ",decimal);
+console.log("La cadena de caracter es: ",caracter);
+console.log("El valor logico es: ",logico);
+
+//Variable constante
+const pi = 3.14;
+
+console.log("La variable la constante es: ",pi);
+
+//Operaciones Aritmaticas
+//Suma
+let numero1=10;
+let numero2=20;
+let suma=numero1+numero2;
+
+console.log("suma1 = ",suma);
+console.log("suma2 = ",numero1+numero2);
+
+//Resta
+console.log("Resta = ",numero1-numero2);
+//Multiplicacion
+console.log("Multiplicacion = ",numero1*numero2);
+//Division
+console.log("Disvision = ",numero1/numero2);
+//Potencia
+console.log("Potencia = ",numero1^numero2);
+//Raiz
+console.log("Raiz = ",numero1numero2);
